9a68ea09d9e9139835a9a233092130652f43d2a5
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-16 15:33:31 +0300

    added score to how much get the player

--- app.py
+++ app.py
@@ -6,9 +6,26 @@ if playing != "yes":
     quit()
     
 print("okay! Let's play :)")
+socre = 0
 
 answer = input("what does CPU stand for? ")
 if answer == "central processing unit":
     print("Correct!")
+    socre += 1
 else:
     print("Incorrect!")
+answer = input("what is the first number? ")
+if answer == "one":
+    print("Correct!")
+    socre += 1
+else:
+    print("Incorrect!")
+answer = input("what is 1 + 1? ")
+if answer == "two":
+    print("Correct!")
+    socre += 1
+else:
+    print("Incorrect!")
+
+print("you got " + str(socre) + " question correct")
+print("you got " + str((socre / 3) * 100) + "%.")
